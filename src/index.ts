c9cbb568bad84d5b7180dc3b47e843b4ecd0ae7a
Nelson Cortes <nelsondmondragon@gmail.com>
2021-12-15 00:40:56 -0500

    Funciones basicas

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,17 @@
     ===== Código de TypeScript =====
 */
 
+function sumar(a:number,b:number): number{
+    return a+b;
+}
+
+const sumarArrow = (a:number,b:number):number => {
+    return a+b;
+}
+
+function multiplicar(numero:number,otroNumero?:number,base:number = 2):number{
+    return numero*base;
+}
+
+console.log(multiplicar(10,20));
+console.log(sumar(10,12));
