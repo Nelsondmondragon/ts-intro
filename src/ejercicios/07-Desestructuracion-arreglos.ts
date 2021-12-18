cdb43ed6686fec643d8d32f99c494725d16a0a4e
Nelson Cortes <nelsondmondragon@gmail.com>
2021-12-18 18:00:42 -0500

    Desestructuracion de arreglos

--- src/ejercicios/07-Desestructuracion-arreglos.ts
+++ src/ejercicios/07-Desestructuracion-arreglos.ts
@@ -1,11 +1,6 @@
-/*
-    ===== Código de TypeScript =====
-*/
-
 const dbz: string[] =['Goku', 'Vegeueta','Trunks'];
 const [goku,,trunsk] = dbz;
 
 console.log('Personaje 1:', goku);
 console.log('Personaje 2:',dbz[0]);
 console.log('Personaje 3:',trunsk);
-
